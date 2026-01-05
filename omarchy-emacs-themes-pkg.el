474c0e9bbe0c744bed2c3633e8de2c2976c0a297
Michael J. Seiter <78192+michaeljseiter@users.noreply.github.com>
2026-01-04 19:09:08 -0600

    Add screenshots

--- omarchy-emacs-themes-pkg.el
+++ omarchy-emacs-themes-pkg.el
@@ -1,5 +1,9 @@
 ;;; omarchy-emacs-themes-pkg.el --- Package metadata for omarchy-emacs-themes -*- lexical-binding: t; -*-
 
+;;; Commentary:
+;;; Package metadata for omarchy-emacs-themes
+
+;;; Code:
 (define-package "omarchy-emacs-themes" "0.2.0"
   "Custom Emacs themes for the Omarchy desktop environment"
   '((emacs "27.1"))
